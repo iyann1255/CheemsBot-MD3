d19b6d0da2f53811a7c473b96a79c37ed81324de
iyann1255 <111185751+iyann1255@users.noreply.github.com>
2022-08-20 10:52:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,26 +24,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc"
-global.ytname = "YT:ARULL OFC " //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
-global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
+global.owner = ['6289516947204'] //ur owner number
+global.ownername = "iyan-store"
+global.ytname = "https://mez.ink/botwhatsapp " //ur yt chanel name
+global.socialm = "grup : https://mez.ink/botwhatsapp" //ur github or insta name
+global.location = "Indonesia, jawa barat, bekasi" //ur location
 
 //bot bombdy
-global.botname = "ArullBotz" //ur bot name
-global.websitex = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //ur website
+global.botname = "https://mez.ink/botwhatsapp" //ur bot name
+global.websitex = "https://mez.ink/botwhatsapp" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
 global.packname = "Sticker By"
-global.author = "ArullBotz"
+global.author = "ʀᴄᴛɪ ʙᴏᴛ"
 global.themeemoji = "📀"
 global.reactmoji = "🗿"
-global.ownertag = ['ArullOfc'] //ur owner tag
-global.ownernummenu = ['6285822347348'] //ur owner number in menu and all
-global.watermark = "ArullBotz" //ur watermark
-global.botscript = "https://youtu.be/Gr6yuCbSGTI"
-global.linkz1 = "https://youtu.be/Gr6yuCbSGTI"
-global.linkz2 = "https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKASl"
+global.ownertag = ['iyan-store'] //ur owner tag
+global.ownernummenu = ['6289516947204'] //ur owner number in menu and all
+global.watermark = "ʀᴄᴛɪ ʙᴏᴛ" //ur watermark
+global.botscript = "https://mez.ink/botwhatsapp"
+global.linkz1 = "https://mez.ink/botwhatsapp"
+global.linkz2 = "https://mez.ink/botwhatsapp"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
